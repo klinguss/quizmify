d58912e36bcf3ad543e68dc5c2b1def88db16a2f
L <lol@gmail.com>
2023-08-09 15:18:12 +0700

    Create Quiz Form

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,7 +1,17 @@
+import QuizCreation from "@/components/QuizCreation";
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
 import React from "react";
 type Props=[];
-const Quiz = (props: Props) => {
-    return <div>Quiz</div>;
-}
+export const metadata = {
+    title: "Quiz | Quizmify",
+};
+const Quiz = async (props: Props) => {
+    const session = await getAuthSession();
+    if (!session?.user){
+        redirect("/")
+    }
+    return <QuizCreation/>;
+};
  
 export default Quiz;
