eee0176e3d4c1a31d2c88212f21d17594353239d
L <lol@gmail.com>
2023-08-11 16:18:27 +0700

    Open-ended

--- src/app/api/checkAnswers/route.ts
+++ src/app/api/checkAnswers/route.ts
@@ -2,6 +2,8 @@ import { prisma } from "@/lib/db";
 import { checkAnswerSchema } from "@/schemas/quiz";
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
+import { compareTwoStrings } from "string-similarity";
+//npm i string-similarity + npm i --save-dev @types/string-similarity
 export async function POST(req: Request, res: Response) {
     try {
         const body = await req.json();
@@ -35,7 +37,21 @@ export async function POST(req: Request, res: Response) {
             });
             return NextResponse.json({
                 isCorrect,
-            }, { status: 200 })
+            }, { status: 200 });
+        } else if (question.questionType === 'open_ended') {
+            let percentageSlimilar = compareTwoStrings(userAnswer.toLowerCase().trim(), question.answer.toLowerCase().trim());
+            percentageSlimilar = Math.round(percentageSlimilar * 100);
+            await prisma.question.update({
+                where: { id: questionId },
+                data: {
+                    percentageCorrect: percentageSlimilar
+                },
+            });
+            return NextResponse.json(
+                {
+                    percentageSlimilar
+                }, { status: 200 }
+            );
         }
     } catch (error) {
         if (error instanceof ZodError) {
